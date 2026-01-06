4685409f1f9d8f2c3ab1fc278e8b1eef96330ea0
Sharmila <murisettysharmila28@gmail.com>
2026-01-06 12:39:15 -0500

    SQL Case
    Basic Case statement and Case in Group by

---
+++ case.sql
@@ -0,0 +1,30 @@
+﻿USE AnalyticIQW10;
+GO
+-- Q1. From the Employee table, show: EmpName, Age. Create a new column AgeCategory:
+-- where: Age < 30 → 'Young', Age between 30 and 40 → 'Mid', Age > 40 → 'Senior'
+
+Select EmpName,Age,
+Case 
+   when Age < 30 then 'Young'
+   when Age >= 30 and Age <=40 then 'Mid'
+   else 'Senior'
+End as AgeCategory
+from Employee;
+
+-- Q2. Group Employees based on their Salary: Low salary <60K, Mid Salary between 60k-10k, High Salary > 100k. Show Salary Category and TotalEmployees
+
+Select
+Case
+    when Salary.Amount < 60000 then 'Low Salary'
+	when Salary.Amount >= 60000 and Salary.Amount <= 100000 then 'Mid Salary'
+	else 'High Salary'
+End as SalaryCategory,
+count(Employee.EmpID) as TotalEmployees
+from Salary
+join Employee on Employee.SalaryID = Salary.SalaryID
+group by 
+      Case
+    when Salary.Amount < 60000 then 'Low Salary'
+	when Salary.Amount >= 60000 and Salary.Amount <= 100000 then 'Mid Salary'
+	else 'High Salary'
+End;
